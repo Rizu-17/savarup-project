fc81dc24607b519934c2619eb3f28dc317f69f67
unknown <rezwan2ulhaque@gmail.com>
2022-03-26 20:47:07 +0600

    set up notice page with jsonplaceholder api

---
+++ js/notice.js
@@ -0,0 +1,27 @@
+let notices = document.querySelector(".notices");
+let output = document.querySelector(".output");
+
+async function getNotices() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  return data;
+}
+
+function getOutputHTML(data) {
+  let outputHTML = "";
+  let trimmedData = data.slice(0, 5);
+  for (let post in trimmedData) {
+    outputHTML += `<p>
+        <b>${data[post].title}</b>:<br /> ${data[post].body}
+      </p>`;
+  }
+  return outputHTML;
+}
+
+async function renderNotices() {
+  const data = await getNotices();
+  const outputHTML = getOutputHTML(data);
+  output.innerHTML = outputHTML;
+}
+
+renderNotices();
